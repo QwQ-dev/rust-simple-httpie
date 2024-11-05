9e5b190f8156288837c5d80f1ac4c03765c937b2
QwQ-dev <qwqdev@gmail.com>
2024-11-05 18:04:53 +0800

    Done.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use anyhow::{anyhow, Result};
 use clap::Parser;
+use colored::Colorize;
+use mime::Mime;
+use reqwest::Client;
+use std::collections::HashMap;
 use std::str::FromStr;
 use url::Url;
 
@@ -27,7 +31,7 @@ struct Post {
     #[arg(short, value_parser = parse_url)]
     url: String,
 
-    #[arg(short, value_parser = parse_kv_pair)]
+    #[arg(short, value_parser = parse_kv_pair, value_delimiter = ',')]
     body: Vec<KvPair>,
 }
 
@@ -58,7 +62,87 @@ fn parse_url(url: &str) -> Result<String> {
     Ok(String::from(Url::parse(url)?))
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let opts: Opts = Opts::parse();
-    println!("{:?}", opts);
+
+    let client = Client::new();
+    let result = match opts.subcommands {
+        SubCommands::Get(ref args) => get(client, args).await?,
+        SubCommands::Post(ref args) => post(client, args).await?,
+    };
+
+    Ok(result)
+}
+
+async fn get(client: Client, get: &Get) -> Result<()> {
+    let response = client.get(&get.url).send().await?;
+    print_all(response).await?;
+    Ok(())
+}
+
+async fn post(client: Client, post: &Post) -> Result<()> {
+    let mut map = HashMap::new();
+
+    for kv_pair in post.body.iter() {
+        let key = &kv_pair.k;
+        let value = &kv_pair.v;
+        map.insert(key, value);
+    }
+
+    let response = client.post(&post.url).json(&map).send().await?;
+    print_all(response).await?;
+
+    Ok(())
+}
+
+fn get_content_type(response: &reqwest::Response) -> Option<Mime> {
+    response.headers()
+        .get(reqwest::header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
+
+async fn print_all(response: reqwest::Response) -> Result<()> {
+    print_status(&response);
+    print_header(&response);
+
+    let mime = get_content_type(&response);
+    let body = response.text().await?;
+
+    print_body(mime, &body);
+
+    Ok(())
+}
+
+fn print_status(response: &reqwest::Response) {
+    let version = response.version();
+    let status_code = response.status();
+
+    let formated_version = format!("{:?}", version).to_string().white();
+    let formated_status_code = format!("{:?}", status_code).to_string().black();
+
+    println!("{} {}", formated_version, formated_status_code);
+}
+
+fn print_header(response:& reqwest::Response) {
+    let headers = response.headers();
+
+    for (name, value) in headers.iter() {
+        let formated_name = format!("{}: ", name).yellow();
+        let formated_value = format!("{:?}", value).blue();
+        println!("{}{}", formated_name, formated_value);
+    }
+}
+
+fn print_body(option_mime: Option<Mime>, body: &String) {
+    match option_mime {
+        Some(v) => {
+            if v == mime::APPLICATION_JSON {
+                println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+            }
+        }
+        _ => {
+            println!("{}", body);
+        }
+    }
 }
